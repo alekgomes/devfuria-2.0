a891ec4c9ec948e084a2a19f7c092a107fd22bdc
alekgomes <lucas.alek93@gmail.com>
2018-09-08 21:15:39 -0300

    add except

--- modulo-03/quatro-operacoes-matematicas.py
+++ modulo-03/quatro-operacoes-matematicas.py
@@ -22,7 +22,7 @@ def multiplicar(num1, num2):
 def subtrair(num1, num2):
 	return num1 - num2
 
-def dividir(num1, num2):
+def dividir(num1, num2): 		
 	return num1 / num2
 
 if escolha == 1:	
@@ -34,8 +34,13 @@ elif escolha == 2:
 elif escolha == 3:	
 	print(f'A subtração é igual a {subtrair(num1, num2)}')
 
-elif escolha == 4:	
-	print(f'A divisão é igual a {dividir(num1, num2)}')
+elif escolha == 4:
+	try:
+		print(f'A divisão é igual a {dividir(num1, num2)}')
+	except ZeroDivisionError:
+		print('Impossível dividir por 0')
+
+	
 
 assert(20  == somar(10, 10))
 assert(100 == multiplicar(10, 10))
